9295f9e1aac86c267d7d3cd08ee985b54390fe53
Jiri Dohnalek <thinkabout3e@googlemail.com>
2017-11-14 21:31:01 +0000

    Update ical.py

--- ical.py
+++ ical.py
@@ -1,17 +1,14 @@
 """
 Generate iCalendar Valid File
 Copyright (C) 2017  Jiri Dohnalek
-
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 2 of the License, or
 (at your option) any later version.
-
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
-
 You should have received a copy of the GNU General Public License along
 with this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
@@ -24,11 +21,7 @@ CRLF = '\r\n'
 
 def print_event(e):
     """ print event """
-    if not e.special:
-        print 'Standard .. {} {} {} {} {}'.format(e.module_code, e.name, e.type, e.start, e.end)
-    else:
-        print 'Special .. {} {} {} {}'.format(e.name, e.type, e.start, e.end)
-
+    print 'Event ... {} {} {}'.format(e.name, e.start, e.end)
 
 def generate_ical(events, debug=False):
     """ Generate iCalendar file from all events """
@@ -36,37 +29,28 @@ def generate_ical(events, debug=False):
     ical += 'VERSION:2.0' + CRLF
     ical += 'PRODID:-//hacksw/handcal//NONSGML v1.0//EN' + CRLF
 
-    # Iterate all event types (lecture, labs, special events)
-    for event_types in events:
+    # Iterate over each event
+    for event in events:
 
-        # Iterate over each event
-        for event in event_types:
+        ical += 'BEGIN:VEVENT{}'.format(CRLF)
+        ical += 'DTSTART:{}{}'.format(event.start, CRLF)
+        ical += 'DTEND:{}{}'.format(event.end, CRLF)
+        ical += 'ORGANIZER:{}{}'.format('University of Liverpool',CRLF)
+        ical += 'SUMMARY: {}{}'.format(event.name, CRLF)
+        ical += 'LOCATION:{}{}'.format('Liverpool UK', CRLF)
+        ical += 'UID:{}{}'.format(event.uid, CRLF)
+        ical += 'TZID:{}{}'.format('Europe/London', CRLF)
 
-            ical += 'BEGIN:VEVENT{}'.format(CRLF)
-            ical += 'DTSTART:{}{}'.format(event.start, CRLF)
-            ical += 'DTEND:{}{}'.format(event.end, CRLF)
-            ical += 'ORGANIZER:{}{}'.format('University of Liverpool',CRLF)
+        # ALARM
+        ical += 'BEGIN:VALARM{}'.format(CRLF)
+        ical += 'TRIGGER:-PT5M{}'.format(CRLF)
+        ical += 'ACTION:DISPLAY{}'.format(CRLF)
+        ical += 'END:VALARM{}'.format(CRLF)
+        ical += 'END:VEVENT{}'.format(CRLF)
 
-            if not event.special:
-                ical += 'SUMMARY: {} {} ({}){}'.format(event.module_code, event.name, event.type, CRLF)
-
-            else:
-                ical += 'SUMMARY: {}{}'.format(event.name, CRLF)
-
-            ical += 'LOCATION:{}{}'.format('Liverpool UK', CRLF)
-            ical += 'UID:{}{}'.format(event.uid, CRLF)
-            ical += 'TZID:{}{}'.format('Europe/London', CRLF)
-
-            # ALARM
-            ical += 'BEGIN:VALARM{}'.format(CRLF)
-            ical += 'TRIGGER:-PT5M{}'.format(CRLF)
-            ical += 'ACTION:DISPLAY{}'.format(CRLF)
-            ical += 'END:VALARM{}'.format(CRLF)
-            ical += 'END:VEVENT{}'.format(CRLF)
-
-            # Debug
-            if debug:
-                print_event(event)
+        # Debug
+        if debug:
+            print_event(event)
 
     ical += 'END:VCALENDAR' + CRLF
 
